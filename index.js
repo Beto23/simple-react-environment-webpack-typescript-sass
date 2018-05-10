92aa4919d5f5afdad69f5b0c2dd023326a530097
beto23 <jooseplugo@gmail.com>
2018-05-10 17:42:11 -0500

    Add editorconfig

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var express = require('express')
-var path = require('path');
-var app = express()
+var express = require("express");
+var path = require("path");
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(path.join(__dirname, 'docs/')), (req, res) => {
-  res.sendFile('index.html', { root: __dirname + '/docs' }, (err) => {
-    if(err) {
-      res.status(500).send(err);
-    }
-  });
+app.set("port", process.env.PORT || 5000);
+app.use(express.static(path.join(__dirname, "docs/")), (req, res) => {
+    res.sendFile("index.html", { root: __dirname + "/docs" }, err => {
+        if (err) {
+            res.status(500).send(err);
+        }
+    });
 });
 
-var server = app.listen(app.get('port'), function() {
-  var port = server.address().port;
+var server = app.listen(app.get("port"), function() {
+    var port = server.address().port;
 });
